6a491f0580f492c983b36a400effe338358d02c1
wisTunnel <se.closer@gmail.com>
2020-02-18 22:16:22 -0800

    update js package

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
 
 // preloader
 $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets
+    $('.preloader').fadeOut(1000); // set duration in brackets    
 });
 
-/* HTML document is loaded. DOM is ready.
+/* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
 $(function(){
 
@@ -18,11 +18,11 @@ $(function(){
   // ------- JQUERY PARALLAX ---- //
   function initParallax() {
     $('#home').parallax("100%", 0.1);
-    $('#tea').parallax("100%", 0.3);
-    $('#food').parallax("100%", 0.3);
+    $('#gallery').parallax("100%", 0.3);
     $('#menu').parallax("100%", 0.2);
-    $('#offer').parallax("100%", 0.3);
+    $('#team').parallax("100%", 0.3);
     $('#contact').parallax("100%", 0.1);
+
   }
   initParallax();
 
@@ -31,19 +31,10 @@ $(function(){
         $(".navbar-collapse").collapse('hide');
     });
 
-  // 隨螢幕寬度調整大小, 可能會有點變形
-  // $('.gallery img').width(screen.width * 0.23);
-  // $('.gallery img').height(screen.width * 0.25);
-
   // NIVO LIGHTBOX
-  $('#tea a').nivoLightbox({
-        effect: 'fadeScale',
-    });
-  $('#food a').nivoLightbox({
-        effect: 'fadeScale',
-    });
-  $('#menu a').nivoLightbox({
+  $('#gallery a').nivoLightbox({
         effect: 'fadeScale',
     });
 
 });
+
